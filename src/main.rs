30aa645d44cca6f5a8ccfad319857cdcdf09c546
Artur Das <a.daschevici@gmail.com>
2025-02-27 08:05:12 +0100

    Update usage of the metrics

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use crate::model::QueryRoot;
+use crate::observability::metrics::{create_prometheus_recorder, track_metrics};
 use crate::routes::{graphql_handler, graphql_playground, health};
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
-use axum::{Router, extract::Extension, routing::get};
+use axum::{Router, Server, extract::Extension, middleware, routing::get};
+use std::future::ready;
 
 mod model;
+mod observability;
 mod routes;
 
 #[tokio::main] // (1)
 async fn main() {
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    let prometheus_recorder = create_prometheus_recorder(); // (1)
     let app = Router::new()
         .route("/", get(graphql_playground).post(graphql_handler))
         .route("/health", get(health))
+        .route("/metrics", get(move || ready(prometheus_recorder.render()))) // (1)
+        .route_layer(middleware::from_fn(track_metrics)) // (2)
         .layer(Extension(schema)); // (2)
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
         .await
         .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
 }
